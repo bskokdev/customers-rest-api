ad6f06a355e628e1299aff4d9f278060d04a78ea
Boris Skok <boris.skok@outlook.com>
2024-05-18 00:03:18 +0200

    feat: add a fallback port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
