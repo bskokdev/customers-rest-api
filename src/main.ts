7f8765f8b1df476f539c1aa10fcb5637bfe45981
Boris Skok <boris.skok@outlook.com>
2024-05-17 22:41:40 +0200

    feat: read port from env variable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap().then(() => {
+  console.log(`App running at port: ${port}`);
+});
