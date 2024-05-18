beba36f4dda9e3e80080b7f5df04e1456d3dd725
Boris Skok <boris.skok@outlook.com>
2024-05-18 12:05:28 +0200

    refactor: improved customer validation

--- src/customer/service/customer.service.ts
+++ src/customer/service/customer.service.ts
@@ -1,10 +1,11 @@
 import { ConflictException, Injectable, Logger, NotFoundException } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { Customer } from '../model/customer.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BasicCustomerInfo, CreateCustomerRequest } from '../dto/customer-dto';
 import { customerToBasicInfo } from '../mapper/customer.mapper';
 import { UUID } from '../../shared/types/uuid.type';
+import { CommandUtils } from 'typeorm/commands/CommandUtils';
 
 @Injectable()
 export class CustomerService {
@@ -31,29 +32,11 @@ export class CustomerService {
     return customer;
   }
 
-  async doesCustomerExist(email: string, phone: string): Promise<boolean> {
-    const existingCustomer = await this.customerRepository.findOne({
-      where: [{ email }, { phone }],
-    });
-    return !!existingCustomer;
-  }
-
-  normalizeCreateDTO(customer: CreateCustomerRequest): CreateCustomerRequest {
-    return {
-      firstName: customer.firstName.trim(),
-      lastName: customer.lastName.trim(),
-      email: customer.email.trim(),
-      phone: customer.phone.replace(/\s+/g, '').trim(),
-    };
-  }
-
   async create(newIncomingCustomer: CreateCustomerRequest): Promise<Customer> {
     const normalizedIncomingCustomer = this.normalizeCreateDTO(newIncomingCustomer);
     const { email, phone } = normalizedIncomingCustomer;
-    if (await this.doesCustomerExist(email, phone)) {
-      this.logger.error(`Customer with ${email} or phone ${phone} already exists`);
-      throw new ConflictException(`Customer with email ${email} or phone ${phone} already exists`);
-    }
+    await this.checkIfEmailInUse(email);
+    await this.checkIfPhoneInUse(phone);
 
     const createdCustomer = this.customerRepository.create(normalizedIncomingCustomer);
     const savedCustomer = await this.customerRepository.save(createdCustomer);
@@ -63,9 +46,41 @@ export class CustomerService {
 
   async update(id: UUID, updatedCustomer: Partial<Customer>): Promise<Customer> {
     const customer = await this.findDetailedCustomerById(id);
+    const { email, phone } = updatedCustomer;
+
+    await this.checkIfEmailInUse(email, id);
+    await this.checkIfPhoneInUse(phone, id);
+
     Object.assign(customer, updatedCustomer);
     const savedCustomer = await this.customerRepository.save(customer);
     this.logger.debug(`Updated customer with ID: ${savedCustomer.id}`);
     return savedCustomer;
+  }
+
+  private normalizeCreateDTO(customer: CreateCustomerRequest): CreateCustomerRequest {
+    return {
+      firstName: customer.firstName.trim(),
+      lastName: customer.lastName.trim(),
+      email: customer.email.trim(),
+      phone: customer.phone.replace(/\s+/g, '').trim(),
+    };
+  }
+
+  private async checkIfEmailInUse(email: string, excludeId?: UUID): Promise<void> {
+    const whereClause = excludeId ? { email, id: Not(excludeId) } : { email };
+    const existingCustomerWithEmail = await this.customerRepository.findOne({ where: whereClause });
+    if (existingCustomerWithEmail) {
+      this.logger.error(`Customer with email ${email} already exists.`);
+      throw new ConflictException(`Customer with email ${email} already exists.`);
+    }
+  }
+
+  private async checkIfPhoneInUse(phone: string, excludeId?: UUID): Promise<void> {
+    const whereClause = excludeId ? { phone, id: Not(excludeId) } : { phone };
+    const existingCustomerWithPhone = await this.customerRepository.findOne({ where: whereClause });
+    if (existingCustomerWithPhone) {
+      this.logger.error(`Customer with phone ${phone} already exists.`);
+      throw new ConflictException(`Customer with phone ${phone} already exists.`);
+    }
   }
 }
